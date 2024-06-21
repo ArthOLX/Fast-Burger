700ad156934ab8addbb6cb0f70518b3de0b14736
Arthur Vaz <tutuvaz1980@gmail.com>
2024-06-21 20:04:23 -0300

    teste commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let usuario = prompt("Olá, seja bem vindo(a) ao FASTburguer ! Qual o seu nome?");
+
+let notification = confirm(`${usuario}, gostaria de receber nossas notificações ?`);
+
+alert(`Seja bem vindo(a), ${usuario}!`);
